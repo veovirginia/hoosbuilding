8603a4c781b8979c78a9d2701e68cc97a87e5da6
alexbckr <amb3yn@virginia.edu>
2022-05-19 00:29:36 +0100

    disabling these buttons till the pages look prettier

--- components/HeaderCompanyOptions.js
+++ components/HeaderCompanyOptions.js
@@ -5,8 +5,8 @@ export default function HeaderCompanyOptions(props) {
     return (
         <div className={styles.companyOptions}>
             <p className={styles.optionsTagline}>HoosBuilding tracks the community's greatest projects - from garage to IPO.</p>
-            <div className={styles.addCompany} onClick={() => Router.push("/add")}>Add a company</div>
-            <div className={styles.updateCompany}>Update a company</div>
+            <div className={styles.addCompany} onClick={console.log("add pushed")}>{/*onClick={() => Router.push("/add")}*/}Add a company</div> 
+            <div className={styles.updateCompany} onClick={console.log("update pushed")}>Update a company</div>
         </div>
     )
 }
