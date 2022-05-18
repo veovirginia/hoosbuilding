f03b0c4bf76f78b293c5b57f46e342f787e93137
alexbckr <amb3yn@virginia.edu>
2022-05-18 16:15:26 +0100

    add founding year filter

--- components/Filters.js
+++ components/Filters.js
@@ -1,5 +1,29 @@
 import styles from "../styles/Filters.module.css";
 import CustomListbox from "./CustomListbox";
+import { useState } from "react";
+
+//prereq: since should be an integer from before the current year
+function generateJSONOfYears(since) {
+    var max = new Date().getFullYear()
+    var min = since
+    console.log("max: " + max)
+    console.log("min: " + min)
+    var years = []
+  
+    for (var i = max; i >= min; i--) {
+      years.push(i)
+    }
+    
+
+    var toReturn = []
+    for (var i in years) {
+        toReturn.push({id: years[i], name: years[i]})
+    }
+
+    console.log(toReturn)
+
+    return toReturn
+}
 
 export default function Filters(props) {
 
@@ -19,7 +43,7 @@ export default function Filters(props) {
       </div>
       <div className={styles.filterGroupLeft}>
         <div className={styles.inputLabel}>Founding Year</div>
-        <input className={styles.input}></input>
+        <CustomListbox items={generateJSONOfYears(2000)}/>
       </div>
       <div className={styles.filterGroup}>
         <div className={styles.inputLabel}>Funding Stage</div>
