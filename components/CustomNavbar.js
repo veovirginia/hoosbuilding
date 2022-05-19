0c151919510086a4262da2134310248cfda6bcc1
alexbckr <amb3yn@virginia.edu>
2022-05-19 22:54:10 +0100

    not bad

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -3,7 +3,7 @@ import styles from '../styles/CustomNavbar.module.css'
 export default function CustomNavbar(props) {
     return (
         <div className={styles.navbar}>
-            <a className={styles.brand} href="/">HoosBuilding</a>
+            <a className={styles.brand} href="/">HoosBuilding 🚀</a>
             <div className={styles.navbarRight}>
                 <div>About</div>
                 <div>Founder Stack</div>
